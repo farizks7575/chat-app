fbebaef299277a561112b7fb18732e44c1f30097
FarizKS <farizzs7575@gmail.com>
2025-06-29 13:01:32 +0530

    Initial commit 1

--- chatapp-backend/DB/connection.js
+++ chatapp-backend/DB/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully!'))
   .catch((err) => console.error('MongoDB connection error:', err));
